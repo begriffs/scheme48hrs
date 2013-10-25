028280bcef93174aff130bb9a2fc17833d129ab0
Joe Nelson <cred+github@begriffs.com>
2013-10-24 18:32:04 -0700

    Initial commit

---
+++ src/scheme.hs
@@ -0,0 +1,15 @@
+module Main where
+
+import System.Environment
+import Text.ParserCombinators.Parsec (parse)
+import SchemeParser
+
+main :: IO ()
+main = do
+  args <- getArgs
+  putStrLn $ readExpr (args !! 0)
+
+readExpr :: String -> String
+readExpr input = case parse SchemeParser.parseExpr "lisp" input of
+  Left err -> "No Match: " ++ show err
+  Right val -> "Found value"
